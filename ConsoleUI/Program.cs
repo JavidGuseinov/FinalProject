b731ea735753b46a589dba30f9083dfbb0c94aa5
javidguseinov <jaweedguseinov@mail.ru>
2022-05-09 17:20:53 +0300

    FinalProject

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new InMemoryProductDal());
 
+
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
